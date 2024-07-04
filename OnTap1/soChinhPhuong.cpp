b16e8affc5e5cbd97ee5b94c985d36168c1b0baa
Vanhhoang2810 <160811741+Vanhhoang2810@users.noreply.github.com>
2024-07-04 08:11:32 +0700

    Create soChinhPhuong.cpp

---
+++ OnTap1/soChinhPhuong.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    if (sqrt(n) * sqrt(n) == n) {
+        cout << "YES";
+    } else {
+        cout << "NO";
+    }
+    return 0;
+}
