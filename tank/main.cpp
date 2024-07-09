162da6c2a063f5c6a7c3f2e1387ddd1407967db9
Hoàng Việt Anh <vanhhoang2810@gmail.com>
2024-07-09 22:33:24 +0700

    xuất hình ảnh

--- tank/main.cpp
+++ tank/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SDL.h>
+#include <SDL_image.h>
 
 using namespace std;
 
@@ -8,12 +9,28 @@ const int SCREEN_HEIGHT = 720;
 
 SDL_Window* window;
 SDL_Renderer* renderer;
-
+SDL_Rect dstTank1, dstTank2;
+SDL_Texture* background = NULL;
+SDL_Texture* tank1 = NULL;
+SDL_Texture* tank2 = NULL;
 
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN, &window, &renderer);
     SDL_SetWindowTitle(window, "Tank Trouble");
+
+    background = IMG_LoadTexture(renderer, "img/background.png");
+    tank1 = IMG_LoadTexture(renderer, "img/tank1.png");
+    tank2 = IMG_LoadTexture(renderer, "img/tank2.png");
+
+    dstTank1.x = 0;
+    dstTank1.y = SCREEN_HEIGHT / 2;
+    dstTank1.w = dstTank1.h = 64;
+
+    dstTank2.x = SCREEN_WIDTH - 64;
+    dstTank2.y = SCREEN_HEIGHT / 2;
+    dstTank2.w = dstTank2.h = 64;
+
 
     SDL_Event ev;
     bool run = true;
@@ -25,10 +42,23 @@ int main(int argc, char* argv[]) {
                 break;
             }
         }
+    SDL_RenderCopy(renderer, background, NULL, NULL);
+    SDL_RenderCopy(renderer, tank1, NULL, &dstTank1);
+    SDL_RenderCopy(renderer, tank2, NULL, &dstTank2);
+
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(10);
+
     }
 
+
+    SDL_DestroyTexture(background);
+    SDL_DestroyTexture(tank1);
+    SDL_DestroyTexture(tank2);
     SDL_DestroyWindow(window);
     SDL_DestroyRenderer(renderer);
+
     SDL_QUIT;
 
 }
